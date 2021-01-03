6296128b77ae56e53e05b75c599eec63c9b5c9f4
Marcus van Winden <marcus.van.winden@icloud.com>
2021-01-03 09:20:56 +0100

    Set up routes as constants

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
+import * as ROUTES from './constants/routes';
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/signin">
+        <Route path={ROUTES.SIGN_IN}>
           <h1>I will be the sign in page</h1>
         </Route>
-        <Route path="/signup">
+        <Route path={ROUTES.SIGN_UP}>
           <h1>I will be the sign up page</h1>
         </Route>
-        <Route path="/browse">
+        <Route path={ROUTES.BROWSE}>
           <h1>I will be the browse page</h1>
         </Route>
-        <Route path="/">
+        <Route path={ROUTES.HOME}>
           <Home />
         </Route>
       </Switch>
