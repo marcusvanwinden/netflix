dc2b946d2bfd0f1cb294dcb11849c1828b7dd0ba
Marcus van Winden <marcus.van.winden@icloud.com>
2021-01-03 09:14:26 +0100

    Add opt-form component

---
+++ src/components/opt-form/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import chevronRight from '../../../images/icons/chevron-right.png';
+import { Container, Input, Button, Text } from './styles/opt-form';
+
+const OptForm = ({ children }) => <Container>{children}</Container>;
+OptForm.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+OptForm.Input = ({ placeholder }) => <Input placeholder={placeholder} />;
+OptForm.Input.propTypes = {
+  placeholder: PropTypes.string.isRequired,
+};
+
+OptForm.Button = ({ children }) => (
+  <Button>
+    {children}
+    <img src={chevronRight} alt="Try Now" />
+  </Button>
+);
+OptForm.Button.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+OptForm.Text = ({ children }) => <Text>{children}</Text>;
+OptForm.Text.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default OptForm;
