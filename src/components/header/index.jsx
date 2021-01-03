a71b8b95b327de3ad4f0a6f17d35d1c144f2e18d
Marcus van Winden <marcus.van.winden@icloud.com>
2021-01-03 12:26:11 +0100

    Add Header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link as ReachRouterLink } from 'react-router-dom';
+import { Background, Container, Logo, ButtonLink } from './styles/header';
+
+const Header = ({ background = true, children }) =>
+  background ? <Background>{children}</Background> : { children };
+Header.propTypes = {
+  children: PropTypes.node.isRequired,
+  background: PropTypes.bool.isRequired,
+};
+
+Header.Frame = ({ children }) => <Container>{children}</Container>;
+Header.Frame.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+Header.Logo = ({ to, src, alt }) => (
+  <ReachRouterLink to={to}>
+    <Logo src={src} alt={alt} />
+  </ReachRouterLink>
+);
+Header.Logo.propTypes = {
+  to: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
+Header.ButtonLink = ({ to, children }) => (
+  <ButtonLink to={to}>{children}</ButtonLink>
+);
+Header.ButtonLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Header;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { OptForm } from '../components';
 import JumbotronContainer from '../containers/jumbotron';
+import HeaderContainer from '../containers/header';
 import FaqsContainer from '../containers/faqs';
 import FooterContainer from '../containers/footer';
 
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <OptForm>
+          <OptForm.Input placeholder="Email Address" />
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Break />
+          <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </OptForm.Text>
+        </OptForm>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
