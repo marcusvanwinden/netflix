7c29fda83784d61a38f3fec8cd32c2eb5591dbe9
Marcus van Winden <marcus.van.winden@icloud.com>
2021-01-05 21:37:51 +0100

    Configure Firebase and its context

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import GlobalStyles from './global-styles';
+import FirebaseContext from './context/firebase';
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById('root')
 );
